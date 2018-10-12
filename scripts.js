f4b6bc016b3dd7da0f5db83b930db3ec345c62a0
Jenniferm221 <jennmclaughlin21@gmail.com>
2018-10-11 20:22:36 -0400

    code to match cards

--- scripts.js
+++ scripts.js
@@ -3,8 +3,8 @@ const cards = document.querySelectorAll('.memory-card');
 let hasFlippedCard = false;
 let firstCard, secondCard;
 
-function flipcard() {
-  this.classList.toggle('flip');
+function flipCard() {
+  this.classList.add('flip');
 
   if (!hasFlippedCard) {
     //first click
@@ -15,10 +15,24 @@ function flipcard() {
     hasFlippedCard = false;
     secondCard = this;
 
+    //do cards match?
+    if(firstCard.dataset.framework === secondCard.dataset.framework) {
+      //it is a match!!!
+      firstCard.removeEventListener('click', flipcard);
+      secondCard.removeEventListener('click', flipcard);
+    } else {
+      setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+      }, 1500);
+    }
 
 
-  }
+
+
+
   }
 }
 
-cards.forEach(card => card.addEventListener('click', flipcard));
+
+cards.forEach(card => card.addEventListener('click', flipCard));
